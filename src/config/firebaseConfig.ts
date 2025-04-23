81c39dbbb742d1e5cdaa22377a07fd93132f5ef9
Nico Perez <nicoperez.dev@gmail.com>
2025-04-23 13:43:21 -0400

    Elimina el archivo de configuración de Firebase de ejemplo y actualiza la configuración de Firebase de producción con nuevas credenciales.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBQkpZYJSrl7QtLHm_7sWJxd48-Wa3lJ0U",
-  authDomain: "smart-tank-85984.firebaseapp.com",
-  projectId: "smart-tank-85984",
-  storageBucket: "smart-tank-85984.firebasestorage.app",
-  messagingSenderId: "211137212888",
-  appId: "1:211137212888:web:84018c9c23ba7566c282ad",
+  apiKey: "AIzaSyD9jLffS16pNp6xnJ0Fbzt67mMy05_lWtU",
+  authDomain: "smart-tank-f5eff.firebaseapp.com",
+  projectId: "smart-tank-f5eff",
+  storageBucket: "smart-tank-f5eff.firebasestorage.app",
+  messagingSenderId: "1049365398105",
+  appId: "1:1049365398105:web:249f1518c7790d654d6748",
 };
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
